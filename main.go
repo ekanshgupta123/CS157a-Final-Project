d192cd2cf2ef5666630d9161601b7e33bd183b49
ekanshgupta123 <ekanshgupta2003@gmail.com>
2023-05-09 17:03:43 -0700

    changes

--- main.go
+++ main.go
@@ -15,17 +15,17 @@ func main() {
     defer db.Close()
 
     // Insert a new applicant into the Address table
-    stmt, err := db.Prepare("INSERT INTO Address (Address_ID, Address, Zip_Code, City, State) VALUES (?, ?, ?, ?, ?)")
+    stmt, err := db.Prepare("INSERT INTO Location (LocationID, Address, City, State) VALUES (?, ?, ?, ?)")
     if err != nil {
         panic(err.Error())
     }
-    _, err = stmt.Exec(1, "123 Street", 94536, "Fremont", "CA")
+    _, err = stmt.Exec(1, "123 Street", "Fremont", "CA")
     if err != nil {
         panic(err.Error())
     }
 
     // Query the Applicant table to retrieve all Address
-    rows, err := db.Query("SELECT * FROM Address")
+    rows, err := db.Query("SELECT * FROM Location")
     if err != nil {
         panic(err.Error())
     }
@@ -33,27 +33,26 @@ func main() {
 
     // Iterate over the result set and print each row
     for rows.Next() {
-        var Address_ID int
+        var LocationID int
         var Address string
-        var Zip_Code int
         var City string
         var State string
-        err = rows.Scan(&Address_ID, &Address, &Zip_Code, &City, &State)
+        err = rows.Scan(&LocationID, &Address, &City, &State)
         if err != nil {
             panic(err.Error())
         }
-        fmt.Printf("Address ID: %d, Address: %s, Zipcode: %d, City: %s, State: %s\n", Address_ID, Address, Zip_Code, City, State)
+        fmt.Printf("Location ID: %d, Address: %s, City: %s, State: %s\n", LocationID, Address, City, State)
     }
 
-    // Insert a new skill into the Skill table
-    stmt, err = db.Prepare("INSERT INTO Skill (Skill_ID, Skill_Name) VALUES (?, ?)")
-    if err != nil {
-        panic(err.Error())
-    }
-    _, err = stmt.Exec(1, "Java")
-    if err != nil {
-        panic(err.Error())
-    }
+    // // Insert a new skill into the Skill table
+    // stmt, err = db.Prepare("INSERT INTO Skill (Skill_ID, Skill_Name) VALUES (?, ?)")
+    // if err != nil {
+    //     panic(err.Error())
+    // }
+    // _, err = stmt.Exec(1, "Java")
+    // if err != nil {
+    //     panic(err.Error())
+    // }
 
     // // Insert a new row into the Applicant_Skill table to link an applicant to a skill
     // stmt, err = db.Prepare("INSERT INTO Applicant_Skill (Applicant_ID, Skill_ID) VALUES (?, ?)")
